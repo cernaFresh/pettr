f285f06eea7efd4629b06659a8c6896bbf3b7996
cernaFresh <cerna_gora@me.com>
2014-05-27 19:12:56 -0600

    added initialize method to pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,14 @@
 class PetsController < ApplicationController
-  
-
   require 'petfinder'
+  def initialize
+    Petfinder.configure do |config|
+      config.api_key = '8d900ecce0d4ac3a40f7184f0cc0cbe8'
+      config.api_secret = '502bdd9ef413243d40296ed4fe9efd2f'
+    end
+
+    @petfinder_client = Petfinder::Client.new
+
+  end
 
   def barnyard
   end
