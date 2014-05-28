12d3bfd8d79ed5bd7fcc06f746a3e1a52a7f701c
cernaFresh <cerna_gora@me.com>
2014-05-27 21:45:59 -0600

    added images and methods to generate metatag info

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,48 +13,87 @@ class PetsController < ApplicationController
     @zipcode = 81501
   end
 
+  def set_meta_info(animal)
+    @meta_description = 'Find ' + animal +  ' available for adoption in Grand Junction, Colorado and throughout the western slope'
+  end
+
+  def get_animal_type(animal)
+    case animal
+    when "Barnyard Animals"
+      animal_type = "barnyard"
+    when "Birds"
+      animal_type = "bird"
+    when "Cats"
+      animal_type = "cat"
+    when "Dogs"
+      animal_type = "dog"
+    when "Horses"
+      animal_type = "horse"
+    when "Pigs"
+      animal_type = "pig"
+    when "Reptiles"
+      animal_type = "reptiles"
+    when "Small and Furry Animals"
+      animal_type = "smallfurry"
+    else
+      animal_type = "cat"
+    end
+    animal_type
+  end
+
   def set_petfinder(animal)
-    @petfinder = @petfinder_client.find_pets(animal, @zipcode)
+    set_meta_info(animal)
+    animal_type = get_animal_type(animal)
+    @petfinder = @petfinder_client.find_pets(animal_type, @zipcode)
 
     render :layout => 'application'
   end
 
   public
   def barnyard
-    set_petfinder('barnyard')
+    @animal = 'Barnyard Animals'
+    set_petfinder(@animal)
   end
 
   def bird
-    set_petfinder('bird')
+    @animal = 'Birds'
+    set_petfinder(@animal)
   end
 
   def cat
-    set_petfinder('cat')
+    @animal = 'Cats'
+    set_petfinder(@animal)
   end
 
   def dog
-    set_petfinder('dog')
+    @animal = 'Dogs'
+    set_petfinder(@animal)
   end
 
   def horse
-    set_petfinder('horse')
+    @animal = 'Horses'
+    set_petfinder(@animal)
   end
 
   def pig
-    set_petfinder('pig')
+    @animal = 'Pigs'
+    set_petfinder(@animal)
   end
 
   def reptile
-    set_petfinder('reptile')
+    @animal = 'Reptiles'
+    set_petfinder(@animal)
   end
 
   def smallfurry
-    set_petfinder('smallfurry')
+    @animal = 'Small and Furry Animals'
+    set_petfinder(@animal)
   end
 
   def view
     @petfinder = Array.new
     @petfinder << @petfinder_client.pet(params[:id])
+    @animal = 'view'
 
     render :layout => 'application'
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,55 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Pettr"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title + ' | ' + page_title}"
+    end
+  end
+
+  def meta_tag(meta_tag)
+    "#{meta_tag}"
+  end
+
+  def meta_tag_image(og_image)
+    base_image = 'http://pettr.skytable.net' + image_path("skytable-star.png")
+    if og_image.empty?
+      base_image
+    else
+      "#{'http://pettr.skytable.net' + og_image}"
+    end
+  end
+  def canon_url
+    "#{request.protocol}#{request.host_with_port}#{request.fullpath}"
+  end
+
+  def metatag_animal_image(animal)
+
+    case animal
+    when "Barnyard Animals"
+      animal_image = "barnyard_cube_fb.jpg"
+    when "Birds"
+      animal_image = "bird_cube_fb.jpg"
+    when "Cats"
+      animal_image = "cats_cube_fb.jpg"
+    when "Dogs"
+      animal_image = "dogs_cube_fb.jpg"
+    when "Horses"
+      animal_image = "horses_cube_fb.jpg"
+    when "Pigs"
+      animal_image = "pigs_cube_fb.jpg"
+    when "Reptiles"
+      animal_image = "reptiles_cube_fb.jpg"
+    when "Small and Furry Animals"
+      animal_image = "smallfurry_cube_fb.jpg"
+    else
+      animal_image = "cats_cube_fb.jpg"
+    end
+
+    animal_image
+
+  end
 end
