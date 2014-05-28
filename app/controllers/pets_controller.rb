a5af8277b622a76e0e2fd2859985cdc8330a4f14
cernaFresh <cerna_gora@me.com>
2014-05-27 20:06:01 -0600

    individual animal pages now search for their respective animals, removed initialize method from pets_controller and added load_vars method

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,40 +1,55 @@
 class PetsController < ApplicationController
   require 'petfinder'
-  def initialize
+
+  before_filter :load_vars
+  protected
+  def load_vars
     Petfinder.configure do |config|
       config.api_key = '8d900ecce0d4ac3a40f7184f0cc0cbe8'
       config.api_secret = '502bdd9ef413243d40296ed4fe9efd2f'
     end
 
     @petfinder_client = Petfinder::Client.new
-
+    @zipcode = 81501
   end
 
-  def barnyard
-  end
-
-  def bird
-  end
-
-  def cat
-    @petfinder = @petfinder_client.shelter_pets('CO22')
+  def set_petfinder(animal)
+    @petfinder = @petfinder_client.find_pets(animal, @zipcode)
 
     render :layout => 'application'
   end
 
+  public
+  def barnyard
+    set_petfinder('barnyard')
+  end
+
+  def bird
+    set_petfinder('bird')
+  end
+
+  def cat
+    set_petfinder('cat')
+  end
+
   def dog
+    set_petfinder('dog')
   end
 
   def horse
+    set_petfinder('horse')
   end
 
   def pig
+    set_petfinder('pig')
   end
 
   def reptile
+    set_petfinder('reptile')
   end
 
   def smallfurry
+    set_petfinder('smallfurry')
   end
 
   def view
